194eacc1bb9312663530edccfda7ce5d8354553f
Gabriel Mota <gabrielmota6@hotmail.com>
2022-06-23 20:17:07 -0300

    Completando alguns repositórios

---
+++ orientacao-objetos-interfaces/src/Quadrado.java
@@ -0,0 +1,26 @@
+public class Quadrado extends Figura2D {
+    
+    private int lado;
+
+    /**
+     * @return the lado
+     */
+    public int getLado() {
+        return lado;
+    }
+
+    /**
+     * @param lado the lado to set
+     */
+    public void setLado(int lado) {
+        this.lado = lado;
+    }
+
+    @Override
+    public double calcularArea() {
+        return lado * lado;
+        //return Math.pow(lado, 2);
+    }
+    
+    
+}
